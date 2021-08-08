9e573d759915391a4b522a011f0eef159ef039b2
Miguel Santana H <mi_santa@hotmail.com>
2021-08-07 22:27:31 -0500

    crea pagina de inicio

---
+++ app/config/colors.js
@@ -0,0 +1,7 @@
+export default {
+  primary: "rgba(152, 251, 152, 0.8)",
+  secondary: "rgba(0, 128, 0, 0.8)",
+  black: "#000",
+  white: "#fff",
+  medium: "#6e6969",
+};
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,24 +1,49 @@
 import React from "react";
-import { ImageBackground, StyleSheet, View } from "react-native";
+import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
+
+import colors from "../config/colors";
 
 function WelcomeScreen(props) {
   return (
     <ImageBackground
       style={styles.background}
-      source={require("../assets/background.jpg")}
+      source={require("../assets/wheelChair2.png")}
     >
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../assets/logoGris.png")} />
+        <Text style={{ fontSize: 15 }}>
+          Sell or Donate Used Rehab Medical Equipment
+        </Text>
+      </View>
       <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
     </ImageBackground>
   );
 }
 const styles = StyleSheet.create({
   background: {
     flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
   },
   loginButton: {
-    width: "100",
+    width: "100%",
     height: 70,
-    backgroundColor: "#fc5c65",
+    backgroundColor: colors.primary,
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: colors.secondary,
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
   },
 });
 export default WelcomeScreen;
