44c48ef566a6867a3f53fc2e7c5ee7086dc8ade8
Miguel Santana H <mi_santa@hotmail.com>
2021-08-07 23:41:30 -0500

    incluye los estilos principales

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,40 +1,35 @@
 import React from "react";
-import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
-
-import colors from "../config/colors";
+import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
+import AppButton from "../components/AppButton";
 
 function WelcomeScreen(props) {
   return (
     <ImageBackground
+      blurRadius={10}
       style={styles.background}
-      source={require("../assets/wheelChair2.png")}
+      source={require("../assets/background.jpg")}
     >
       <View style={styles.logoContainer}>
-        <Image style={styles.logo} source={require("../assets/logoGris.png")} />
-        <Text style={{ fontSize: 15 }}>
-          Sell or Donate Used Rehab Medical Equipment
-        </Text>
+        <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+        <Text style={styles.tagline}>Sell What You Don't Need</Text>
       </View>
-      <View style={styles.loginButton}></View>
-      <View style={styles.registerButton}></View>
+      <View style={styles.buttonsContainer}>
+        <AppButton title="Login" />
+        <AppButton title="Register" color="secondary" />
+      </View>
     </ImageBackground>
   );
 }
+
 const styles = StyleSheet.create({
   background: {
     flex: 1,
     justifyContent: "flex-end",
     alignItems: "center",
   },
-  loginButton: {
+  buttonsContainer: {
+    padding: 20,
     width: "100%",
-    height: 70,
-    backgroundColor: colors.primary,
-  },
-  registerButton: {
-    width: "100%",
-    height: 70,
-    backgroundColor: colors.secondary,
   },
   logo: {
     width: 100,
@@ -45,5 +40,11 @@ const styles = StyleSheet.create({
     top: 70,
     alignItems: "center",
   },
+  tagline: {
+    fontSize: 25,
+    fontWeight: "600",
+    paddingVertical: 20,
+  },
 });
+
 export default WelcomeScreen;
